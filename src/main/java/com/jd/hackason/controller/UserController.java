c2ee4624cf8a75736e8ea97ea0a80cdff81fdb0c
joen <qjcsch@163.com>
2016-07-27 18:21:14 +0800

    sss

--- src/main/java/com/jd/hackason/controller/UserController.java
+++ src/main/java/com/jd/hackason/controller/UserController.java
@@ -15,17 +15,24 @@ import com.jd.hackason.service.IUserService;
 public class UserController {
 	@Resource
 	private IUserService iUserService;
-	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+
+	@RequestMapping(value = "/login.action", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	public ResponseEntity<Object> login(@RequestBody User user) throws Exception {
 		System.out.println();
 		User u = iUserService.login(user);
 		System.out.println(u);
-		return new ResponseEntity<Object>(u,HttpStatus.OK);
-		
-		
-	/*	Map<String, Object> map = new HashMap<>();
-		map.put("pos", "3");
-		map.put("msg", "用户类型错误");
-		return new ResponseEntity<Object>(map, HttpStatus.BAD_REQUEST);*/
+		return new ResponseEntity<Object>(u, HttpStatus.OK);
+
+		/*
+		 * Map<String, Object> map = new HashMap<>(); map.put("pos", "3");
+		 * map.put("msg", "用户类型错误"); return new ResponseEntity<Object>(map,
+		 * HttpStatus.BAD_REQUEST);
+		 */
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET, produces = "application/json", consumes = "application/json")
+	public ResponseEntity<Object> test() throws Exception {
+		System.out.println(123456);
+		return new ResponseEntity<Object>(HttpStatus.OK);
 	}
 }
